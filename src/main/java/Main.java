e3467307505e9cdb16b3ac69405ccd4c4e0b6e7a
FelixHultman <karlfelixhultman@gmail.com>
2024-08-22 14:34:14 +0200

    Fixed hours and price output-format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -13,12 +15,16 @@ public class Main {
                 "4. Bästa Laddningstid (4h)\n" +
                 "e. Avsluta");
         System.out.println("Please make your choice in the menu");
-        String choice = scanner.nextLine();
+        String choice = scanner.nextLine().toLowerCase();
         System.out.println("You choose " + choice);
+
+        HourlyPrice[] priceData = null;
 
         switch (choice) {
             case "1":
                 System.out.println("Inmatning");
+                priceData = priceInput(scanner);
+                printPriceData(priceData);
                 break;
             case "2":
                 System.out.println("Min, Max och Medel");
@@ -36,6 +42,52 @@ public class Main {
                 System.out.println("Invalid choice, Pick something else");
                 break;
 
+
         }
+    }
+
+    private static HourlyPrice[] priceInput(Scanner scanner) {
+        HourlyPrice[] priceData = new HourlyPrice[24];
+        for (int i = 0; i < 24; i++) {
+            while (true) {
+                String nextHour = String.format("%02d", (i + 1) % 24);
+                System.out.println("Ange pris för tiden " + String.format("%02d", i) + "-" + nextHour + ": ");
+                String input = scanner.nextLine();
+                try {
+                    int price = Integer.parseInt(input);
+                    if (price < 0) {
+                        System.out.println("Inget negativt pris här, tack.");
+                    } else {
+                        priceData[i] = new HourlyPrice(i, price);
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Ogiltigt inmatning. Ange heltal i öre");
+                }
+            }
+        }
+
+        return priceData;
+
+    }
+
+    private static void printPriceData(HourlyPrice[] priceData) {
+        for (HourlyPrice hp : priceData) {
+            if (hp != null) {
+                int nextHour = (hp.hour + 1) % 24;
+                System.out.println("Timme: " + String.format("%02d", hp.hour) + "-" + String.format("%02d ", nextHour) + "Pris: " + (hp.price) + " Öre");
+            }
+        }
+
+    }
+}
+
+class HourlyPrice {
+    int hour;
+    int price;
+
+    HourlyPrice(int hour, int price) {
+        this.hour = hour;
+        this.price = price;
     }
 }
